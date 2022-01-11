e6e6798500e48a55ec8bfbd2fbb311f6fa4b08cb
Max Milton <max@happycode.ltd>
2022-01-11 13:37:18 +1000

    chore: Fix docker build github workflow env vars

--- .github/workflows/test-docker.sh
+++ .github/workflows/test-docker.sh
@@ -13,8 +13,9 @@ docker run --rm \
   --cap-drop ALL \
   --security-opt no-new-privileges \
   --env CONFIG_PATH="/data/trackx.config.js" \
+  --env DB_SCHEMA_PATH="/data/db/master.sql" \
   --env DEMO_CONFIG_PATH="/data/demo.config.js" \
-  --env DB_PATH="/tmp/db/trackx.db" \
+  --env DEMO_DB_PATH="/tmp/db/trackx.db" \
   --env DEMO_MODE=true \
   --tmpfs /tmp/db:rw,noexec,nodev,nosuid,uid=5063,gid=5063,mode=0700,size=1m \
   --mount type=bind,src="$config_path",dst=/data/trackx.config.js,ro \
